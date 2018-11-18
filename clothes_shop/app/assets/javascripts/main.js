ba8579f837d2a5c64a772ab91d11d822683b4336
apo <apostolos.n.sotiropoulos@gmail.com>
2018-11-18 22:37:15 +0100

    ci js fix svg resize

--- clothes_shop/app/assets/javascripts/main.js
+++ clothes_shop/app/assets/javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('this clothes shop imitator. Ouxou!')
+function fixCartSVGSize() {
+  let cartButton = document.getElementById('cart-button')
+  let cartButtonDimensions = cartButton.getBoundingClientRect()
+  let cartMinDimension = Math.min(cartButtonDimensions.height, cartButtonDimensions.width) - 1
+  let cartMInDimensionString = cartMinDimension + 'px'
+  let embededEl = document.getElementById("sv") 
+  let embededSVG = embededEl.getSVGDocument().getElementById('Capa_1')
+  embededSVG.setAttribute('height', cartMInDimensionString)
+  embededSVG.setAttribute('width', cartMInDimensionString)
+}
+
+window.addEventListener('resize', fixCartSVGSize) 
